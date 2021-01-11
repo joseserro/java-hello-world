880a58c16bb1e29b04c582ccc7ffb1f874026642
user <pedro.mda@gmail.com>
2021-01-11 12:54:40 +0000

    Adicionar .gitignore Modificado main

--- src/pt/serro/test/Main.java
+++ src/pt/serro/test/Main.java
@@ -3,6 +3,6 @@ package pt.serro.test;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-		System.out.println("Olá!")
+		System.out.println("Olá! Tudo bem?");
     }
 }
