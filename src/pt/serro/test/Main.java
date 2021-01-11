e6c0e484862b81db4764b91b9a6d210abbe2d7c1
TiagoMPinto <tiagopinto_27.03@outlook.pt>
2021-01-11 12:12:17 +0000

    Added new feature

--- src/pt/serro/test/Main.java
+++ src/pt/serro/test/Main.java
@@ -3,5 +3,7 @@ package pt.serro.test;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello José!");
+
     }
 }
